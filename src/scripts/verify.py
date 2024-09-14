6287b45202f72d829e1543111520973b5513028d
B Horn <b@horn.uk>
2024-09-14 22:14:35 +0100

    Extending UEFI wait time, another string to look for.
    Signed-off-by: B Horn <b@horn.uk>

--- src/scripts/verify.py
+++ src/scripts/verify.py
@@ -3,7 +3,10 @@ import sys
 
 def main():
     # All good kernels should feature this.
-    want = {'PATCHED KERNEL': 0}
+    want = {
+        'PATCHED KERNEL': 0,
+        'Please press Enter to activate this console.': 0
+    }
     # If we find these, bad patch
     dont_want = {'panic': 0, 'KASAN': 0, 'BUG': 0}
 
@@ -20,12 +23,12 @@ def main():
 
     for key in want:
         if want[key] == 0:
-            print(f'* Missing {key}')
+            print(f'* Missing "{key}"')
             bad = -1
 
     for key in dont_want:
         if dont_want[key] > 0:
-            print(f'* Found {key}')
+            print(f'* Found "{key}"')
             bad = -1
 
     sys.exit(bad)
--- src/test-batch.sh
+++ src/test-batch.sh
@@ -4,6 +4,6 @@ echo $1
 if ! ./src/test.sh 15 bios 2>/dev/null; then
     echo "BIOS FAILED"
 fi
-if ! ./src/test.sh 15 uefi 2>/dev/null; then
+if ! ./src/test.sh 30 uefi 2>/dev/null; then
     echo "UEFI FAILED"
 fi
