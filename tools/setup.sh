fb48bfcff9a7d87fe030699f141a102f0095a22a
B Horn <b@horn.uk>
2026-03-22 01:31:44 +0000

    git cloning easylkb outside of a submodule
    want to be able to move to jj, can't use submodules with it
    
    Signed-off-by: B Horn <b@horn.uk>

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,3 +2,4 @@
 source .venv/bin/activate
 pip install -r requirements.txt
 git clone https://github.com/marin-m/vmlinux-to-elf tools/vmlinux-to-elf
+git clone https://github.com/deepseagirl/easylkb tools/easylkb
