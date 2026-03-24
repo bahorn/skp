0d342029ce27ecf94064cec137801d73c390a186
B Horn <b@horn.uk>
2026-03-24 18:49:11 +0000

    importing klude2
    not used for anything yet, but wanted for later work
    
    Signed-off-by: B Horn <b@horn.uk>

--- tools/setup.sh
+++ tools/setup.sh
@@ -3,3 +3,4 @@ source .venv/bin/activate
 pip install -r requirements.txt
 git clone https://github.com/marin-m/vmlinux-to-elf tools/vmlinux-to-elf
 git clone https://github.com/deepseagirl/easylkb tools/easylkb
+git clone https://github.com/bahorn/klude2 tools/klude2
