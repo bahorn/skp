d312e65dad0b833106892325de81939b36f832dc
B Horn <b@horn.uk>
2026-04-30 18:15:22 +0100

    Switch to my patched version of easylkb
    Need for 7.x support
    
    Signed-off-by: B Horn <b@horn.uk>

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,5 +2,5 @@
 source .venv/bin/activate
 pip install -r requirements.txt
 git clone https://github.com/marin-m/vmlinux-to-elf tools/vmlinux-to-elf
-git clone https://github.com/deepseagirl/easylkb tools/easylkb
+git clone https://github.com/bahorn/easylkb tools/easylkb
 git clone https://github.com/bahorn/klude2 tools/klude2
